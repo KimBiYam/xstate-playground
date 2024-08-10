34bba220195ead92c60abe3b8f9b81ea66821466
Chang Hyun Kim <ckdvk9998@gmail.com>
2024-08-10 21:11:10 +0900

    refactor: refactor unit tests with actor

--- apps/cart/src/state/__tests__/cartState.test.ts
+++ apps/cart/src/state/__tests__/cartState.test.ts
@@ -1,19 +1,22 @@
+import { createActor } from "xstate";
 import { CartItem } from "../../domains/cart/cart";
-import { useCart } from "../cartState";
-import { act, renderHook } from "@testing-library/react";
+import { cartMachine } from "../cartState";
 
 describe("cartState", () => {
+  let actor: ReturnType<typeof createActor<typeof cartMachine>>;
+
   const item1: CartItem = { id: 1, price: 100, stock: 1 };
   const item2: CartItem = { id: 2, price: 100, stock: 1 };
   const item3: CartItem = { id: 3, price: 100, stock: 1 };
 
-  const prepare = () => renderHook(() => useCart());
+  beforeEach(() => {
+    actor = createActor(cartMachine);
+  });
 
   describe("initialize", () => {
     it("should entries is empty", () => {
-      const { result } = prepare();
-      const [state] = result.current;
-      expect(state.context.entries).toEqual([]);
+      actor.start();
+      expect(actor.getSnapshot().context.entries).toEqual([]);
     });
   });
 
@@ -21,169 +24,136 @@ describe("cartState", () => {
     describe("addItem", () => {
       it("should add item when item isn't in cart", () => {
         // given
-        const { result } = prepare();
-        const [_, send] = result.current;
+        actor.start();
 
         // when
-        act(() => {
-          send({ type: "addItem", item: item1 });
-        });
+        actor.send({ type: "addItem", item: item1 });
 
         // then
-        const [state] = result.current;
-        expect(state.context.entries).toEqual([{ item: item1, quantity: 1 }]);
+        expect(actor.getSnapshot().context.entries).toEqual([
+          { item: item1, quantity: 1 },
+        ]);
       });
 
       it("should add quantity when item is already in cart", () => {
         // given
-        const { result } = prepare();
-        const [_, send] = result.current;
+        actor.start();
 
         // when
-        act(() => {
-          send({ type: "addItem", item: item1 });
-          send({ type: "addItem", item: item1 });
-        });
+        actor.send({ type: "addItem", item: item1 });
+        actor.send({ type: "addItem", item: item1 });
 
         // then
-        const [state] = result.current;
-        expect(state.context.entries).toEqual([{ item: item1, quantity: 2 }]);
+        expect(actor.getSnapshot().context.entries).toEqual([
+          { item: item1, quantity: 2 },
+        ]);
       });
 
       it("should change state to isFull when entries is more than 3", () => {
         // given
-        const { result } = prepare();
-        const [_, send] = result.current;
+        actor.start();
 
         // when
-        act(() => {
-          send({ type: "addItem", item: item1 });
-          send({ type: "addItem", item: item2 });
-          send({ type: "addItem", item: item3 });
-        });
+        actor.send({ type: "addItem", item: item1 });
+        actor.send({ type: "addItem", item: item2 });
+        actor.send({ type: "addItem", item: item3 });
 
         // then
-        const [state] = result.current;
-        expect(state.context.entries.length).toBe(3);
-        expect(state.value).toBe("isFull");
+        expect(actor.getSnapshot().context.entries.length).toBe(3);
+        expect(actor.getSnapshot().value).toBe("isFull");
       });
     });
 
     describe("removeItem", () => {
       it("should nothing when cart is empty", () => {
         // given
-        const { result } = prepare();
-        const [_, send] = result.current;
+        actor.start();
 
         // when
-        act(() => {
-          send({ type: "removeItem", itemId: 1 });
-        });
+        actor.send({ type: "removeItem", itemId: 1 });
 
         // then
-        const [state] = result.current;
-        expect(state.context.entries).toEqual([]);
+        expect(actor.getSnapshot().context.entries).toEqual([]);
       });
 
       it("should nothing when itemId not matched", () => {
         // given
-        const { result } = prepare();
-        const [_, send] = result.current;
+        actor.start();
 
         // when
-        act(() => {
-          send({ type: "addItem", item: item1 });
-          send({ type: "removeItem", itemId: 2 });
-          send({ type: "removeItem", itemId: 3 });
-          send({ type: "removeItem", itemId: 4 });
-          send({ type: "removeItem", itemId: 5 });
-        });
+        actor.send({ type: "addItem", item: item1 });
+        actor.send({ type: "removeItem", itemId: 2 });
+        actor.send({ type: "removeItem", itemId: 3 });
+        actor.send({ type: "removeItem", itemId: 4 });
+        actor.send({ type: "removeItem", itemId: 5 });
 
         // then
-        const [state] = result.current;
-        expect(state.context.entries).toEqual([{ item: item1, quantity: 1 }]);
+        expect(actor.getSnapshot().context.entries).toEqual([
+          { item: item1, quantity: 1 },
+        ]);
       });
 
       it("should remove cart when before quantity is 1", () => {
         // given
-        const { result } = prepare();
-        const [_, send] = result.current;
+        actor.start();
 
         // when
-        act(() => {
-          send({ type: "addItem", item: item1 });
-          send({ type: "removeItem", itemId: 1 });
-        });
+        actor.send({ type: "addItem", item: item1 });
+        actor.send({ type: "removeItem", itemId: 1 });
 
         // then
-        const [state] = result.current;
-        expect(state.context.entries).toEqual([]);
+        expect(actor.getSnapshot().context.entries).toEqual([]);
       });
 
       it("should decrease item in cart when before quantity is more than 1", () => {
         // given
-        const { result } = prepare();
-        const [_, send] = result.current;
+        actor.start();
 
         // when
-        act(() => {
-          send({ type: "addItem", item: item1 });
-          send({ type: "addItem", item: item1 });
-          send({ type: "removeItem", itemId: 1 });
-        });
+        actor.send({ type: "addItem", item: item1 });
+        actor.send({ type: "addItem", item: item1 });
+        actor.send({ type: "removeItem", itemId: 1 });
 
         // then
-        const [state] = result.current;
-        expect(state.context.entries).toEqual([{ item: item1, quantity: 1 }]);
+        expect(actor.getSnapshot().context.entries).toEqual([
+          { item: item1, quantity: 1 },
+        ]);
       });
     });
   });
 
   describe("[isFull]", () => {
-    let sut = prepare();
-
     beforeEach(function changeCartToIsFull() {
-      sut = prepare();
-      const [_, send] = sut.result.current;
+      actor.start();
 
-      act(() => {
-        send({ type: "addItem", item: item1 });
-        send({ type: "addItem", item: item2 });
-        send({ type: "addItem", item: item3 });
-      });
+      actor.send({ type: "addItem", item: item1 });
+      actor.send({ type: "addItem", item: item2 });
+      actor.send({ type: "addItem", item: item3 });
     });
 
     describe("addItem", () => {
       it("should nothing", () => {
         // given
-        const [_, send] = sut.result.current;
         const item4: CartItem = { id: 4, price: 100, stock: 1 };
 
         // when
-        act(() => {
-          send({ type: "addItem", item: item4 });
-        });
+        actor.send({ type: "addItem", item: item4 });
 
         // then
-        const [state] = sut.result.current;
-        expect(state.value).toBe("isFull");
-        expect(state.context.entries.length).toBe(3);
+        expect(actor.getSnapshot().value).toBe("isFull");
+        expect(actor.getSnapshot().context.entries.length).toBe(3);
       });
     });
 
     describe("remoteItem", () => {
       it("should change state to default", () => {
         // given
-        const [_, send] = sut.result.current;
 
         // when
-        act(() => {
-          send({ type: "removeItem", itemId: item1.id });
-        });
+        actor.send({ type: "removeItem", itemId: item1.id });
 
         // then
-        const [state] = sut.result.current;
-        expect(state.value).toBe("default");
+        expect(actor.getSnapshot().value).toBe("default");
       });
     });
   });
